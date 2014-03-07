d7d979d64c699e6bbb392a6aceaf861ebaf5dbf8
Ukang'a Dickson <ukanga@gmail.com>
2014-03-07 15:29:10 +0200

    UK: login to drupal server using requests.Session

--- devtrac/libs/drupal.py
+++ devtrac/libs/drupal.py
@@ -4,16 +4,49 @@ from urllib.parse import urljoin
 
 
 DRUPAL_CONNECT_PATH = 'api/system/connect.json'
+DRUPAL_LOGIN_PATH = 'api/user/login.json'
+DRUPAL_LOGOUT_PATH = 'api/user/logout.json'
 
 
 class Drupal(object):
+    headers = {}
     host = None
+    request = None
+
     connect_url = None
+    login_url = None
+    logout_url = None
+
+    token = None
+    session_id = None
+    session_name = None
+    uid = None
 
     def __init__(self, host="http://127.0.0.1"):
         self.host = host
         self.connect_url = urljoin(self.host, DRUPAL_CONNECT_PATH)
+        self.login_url = urljoin(self.host, DRUPAL_LOGIN_PATH)
+        self.logout_url = urljoin(self.host, DRUPAL_LOGOUT_PATH)
 
     def connect(self):
-        self.response = requests.post(self.connect_url)
+        if self.request is not None and self.headers:
+            self.response = self.request.post(self.connect_url,
+                                              headers=self.headers)
+        else:
+            self.response = requests.post(self.connect_url)
         return self.response
+
+    def login(self, username='api_user', password='api_pass'):
+        data = {'username': username, 'password': password}
+        self.request = requests.Session()
+        self.response = self.request.post(self.login_url, data)
+        if self.response.status_code == 200:
+            data = self.response.json()
+            self.uid = data.get('uid')
+            self.token = data.get('token')
+            self.session_id = data.get('sessid')
+            self.session_name = data.get('session_name')
+            self.headers['X-CSRF-Token'] = self.token
+        else:
+            return False
+        return True
--- devtrac/libs/tests/test_drupal.py
+++ devtrac/libs/tests/test_drupal.py
@@ -8,8 +8,31 @@ class TestDrupal(TestCase):
     def setUp(self):
         self.drupal = Drupal()
 
+    def _drupal_login(self):
+        self.assertIsNone(self.drupal.token)
+        self.assertIsNone(self.drupal.session_id)
+
+        self.drupal.login(username='api_user', password='api_pass')
+
+        self.assertEqual(self.drupal.response.status_code, 200)
+        self.assertIsNotNone(self.drupal.token)
+        self.assertIsNotNone(self.drupal.session_id)
+
     def test_connect_anonymous_user(self):
         response = self.drupal.connect()
         self.assertEqual(response.status_code, 200)
         self.assertIn('{"uid":0,"hostname":"127.0.0.1",'
                       '"roles":{"1":"anonymous user"}', response.text)
+
+    def test_login_failed(self):
+        self.drupal.login(username='demo', password='wrong_pass')
+        self.assertTrue(self.drupal.response.status_code, 401)
+
+    def test_login_success(self):
+        self._drupal_login()
+
+    def test_connect_user(self):
+        self._drupal_login()
+        response = self.drupal.connect()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('"name":"api_user"', response.text)
