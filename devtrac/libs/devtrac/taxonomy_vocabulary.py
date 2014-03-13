06be40b044ec1e7bf76abf0a7a1004d1119c2bba
Ukang'a Dickson <ukanga@gmail.com>
2014-03-13 12:35:43 +0300

    UK: add taxonomy vocabulary 1 for location type

---
+++ devtrac/libs/devtrac/taxonomy_vocabulary.py
@@ -0,0 +1,81 @@
+class TAXONOMY_VOCABULARY_1:
+    SCHOOLS = 1
+    HEALTH_FACILITIES = 2
+    GOVERNMENT_OFFICE = 3
+    PRIMARY_GOVERNMENT_SCHOOL = 4
+    SECONDARY_GOVERNMENT_SCHOOL = 5
+    SECONDARY_COMMUNITY_SCHOOL = 6
+    HEALTH_CENTRE_III = 7
+    HEALTH_CENTRE_II = 8
+    WATER_POINTS = 23
+    WELL = 25
+    NGO = 47
+    FAITH_BASED_ORGANISATION = 48
+    OTHER_LOCATIONS = 49
+    HEALTH_CENTRE_IV = 50
+    HOSPITAL = 51
+    OTHER_HEALTH_CENTRE = 52
+    TAP = 53
+    UNKNOWN_WATERPOINT = 55
+    HEALTH_UNIT = 56
+    PRIMARY_COMMUNITY_SCHOOL = 57
+    OFFICE = 58
+    PRIMARY_PRIVATE_SCHOOL = 59
+    SECONDARY_PRIVATE_SCHOOL = 60
+    OTHER_SCHOOL = 61
+    DEEP_BOREHOLE = 191
+    PROTECTED_SPRING = 192
+    PRIMARY_SCHOOL = 195
+    HEALTH_CENTRE = 197
+    CHILD_PROTECTION_COMMITTEE = 200
+    HOUSEHOLD = 201
+    SHOP_OR_MARKET = 202
+    POLICE_STATION = 203
+    BOREHOLE = 204
+    GFS_OR_TAP = 205
+    SECONDARY_SCHOOL = 206
+    SHALLOW_WELL = 207
+    YOUTH_CENTRE = 212
+    PLACE_OF_WORSHIP = 500
+    REFUGEE_CAMP = 842
+    LABELS = {
+        SCHOOLS: u"Schools",
+        HEALTH_FACILITIES: u"Health Facilities",
+        GOVERNMENT_OFFICE: u"Government Office",
+        PRIMARY_GOVERNMENT_SCHOOL: u"Primary Government School",
+        SECONDARY_GOVERNMENT_SCHOOL: u"Secondary Government School",
+        SECONDARY_COMMUNITY_SCHOOL: u"Secondary Community School",
+        HEALTH_CENTRE_III: u"Health Centre III",
+        HEALTH_CENTRE_II: u"Health Centre II",
+        WATER_POINTS: u"Water Points",
+        WELL: u"Well",
+        NGO: u"NGO",
+        FAITH_BASED_ORGANISATION: u"Faith-based Organisation",
+        OTHER_LOCATIONS: u"Other Locations",
+        HEALTH_CENTRE_IV: u"Health Centre IV",
+        HOSPITAL: u"Hospital",
+        OTHER_HEALTH_CENTRE: u"Other Health Centre",
+        TAP: u"Tap",
+        UNKNOWN_WATERPOINT: u"Unknown Waterpoint",
+        HEALTH_UNIT: u"Health Unit",
+        PRIMARY_COMMUNITY_SCHOOL: u"Primary Community School",
+        OFFICE: u"Office",
+        PRIMARY_PRIVATE_SCHOOL: u"Primary Private School",
+        SECONDARY_PRIVATE_SCHOOL: u"Secondary Private School",
+        OTHER_SCHOOL: u"Other School",
+        DEEP_BOREHOLE: u"Deep Borehole",
+        PROTECTED_SPRING: u"Protected Spring",
+        PRIMARY_SCHOOL: u"Primary School",
+        HEALTH_CENTRE: u"Health Centre",
+        CHILD_PROTECTION_COMMITTEE: u"Child Protection Committee",
+        HOUSEHOLD: u"Household",
+        SHOP_OR_MARKET: u"Shop or Market",
+        POLICE_STATION: u"Police Station",
+        BOREHOLE: u"Borehole",
+        GFS_OR_TAP: u"Gfs or Tap",
+        SECONDARY_SCHOOL: u"Secondary School",
+        SHALLOW_WELL: u"Shallow Well",
+        YOUTH_CENTRE: u"Youth Centre",
+        PLACE_OF_WORSHIP: u"Place of Worship",
+        REFUGEE_CAMP: u"Refugee Camp"
+    }
