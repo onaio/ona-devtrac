043768919ce6a85d4851eb4a972a030ac161f59f
Ukang'a Dickson <ukanga@gmail.com>
2014-03-27 21:06:58 +0300

    UK: set-und and code refactor

--- devtrac/libs/devtrac/site_visit.py
+++ devtrac/libs/devtrac/site_visit.py
@@ -36,6 +36,17 @@ class SiteVisit(DrupalNode):
             }
         )
 
+    def set_und(self, field, value):
+        self._node_dict.update(
+            {
+                field: {
+                    'und': [
+                        {'value': value}
+                    ]
+                }
+            }
+        )
+
     def set_public_summary(self, summary):
         self._node_dict.update(
             {
@@ -47,12 +58,19 @@ class SiteVisit(DrupalNode):
             }
         )
 
-    def set_field_trip(self, field_trip_id):
+    def set_target_id(self, field, target):
         self._node_dict.update({
-            'field_ftritem_field_trip': {
-                'und': [{'target_id': field_trip_id}]
+            field: {
+                'und': [{'target_id': target}]
             }
         })
+
+    def set_field_trip(self, field_trip_id):
+        self.set_target_id('field_ftritem_field_trip', field_trip_id)
+
+    def set_place(self, place):
+        if place is not None:
+            self.set_target_id('field_ftritem_place', place)
 
     def set_location(self, location):
         if location is not None:
@@ -61,12 +79,14 @@ class SiteVisit(DrupalNode):
             }})
 
     def set_site_visit(self, date_visited, location_type, narrative, summary,
-                       field_trip_id=None, location=None):
+                       field_trip_id=None, location=None, place=None):
         self.set_date_visited(date_visited)
         self.set_taxonomy_vocabulary(1, location_type)
         self.set_narrative(narrative)
         self.set_public_summary(summary)
         self.set_location(location)
+        self.set_place(place)
+        # self.set_und('field_ftritem_status', 'Submitted')
 
         if field_trip_id:
             self.set_field_trip(field_trip_id)
--- devtrac/libs/tests/test_devtrac.py
+++ devtrac/libs/tests/test_devtrac.py
@@ -46,15 +46,22 @@ class TestDevTrac(TestBase):
 
     def test_create_site_visit(self):
         self._create_field_trip()
+        # self._drupal_login()
+        # self.fieldtrip_node = {'uri': 'http://jenkinsge.mountbatten.net/'
+        #                        'devtracmanual/api/node/14997.json',
+        #                        'nid': '14997'}
         fieldtrip = self.drupal.get_node(self.fieldtrip_node['uri'])
         target_id = u'%s (%s)' % (fieldtrip['title'], fieldtrip['nid'])
+        place = u'%s (%s)' % ('UMSC Office Kotido', '5547')
         st = SiteVisit('Visit at Place A', node_type='ftritem')
         st.set_site_visit(
             '13/03/2014', TAXONOMY_VOCABULARY_1.NGO,
             'Narrative data', 'summary data', target_id,
-            location='34.114095689096 2.9995385028608')
+            location='34.114095689096 2.9995385028608',
+            place=place)
         st._node_dict.update({'nid': fieldtrip})
         node = self.drupal.create_node(st)
         self.assertIsInstance(node, dict)
         self.assertIn('nid', node.keys())
         self.assertIn('uri', node.keys())
+        print(node)
