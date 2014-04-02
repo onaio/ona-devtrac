526d54194dd3ad1a13bf6a2f5b7b6827c8cb5b64
Ukang'a Dickson <ukanga@gmail.com>
2014-04-02 15:48:37 +0300

    UK: tests for roadside and human interest reports

--- devtrac/libs/tests/test_utils.py
+++ devtrac/libs/tests/test_utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import warnings
 
 from devtrac.libs.tests.test_base import TestBase
 from devtrac.libs import utils
@@ -12,6 +13,31 @@ class TestUtils(TestBase):
         fieldtrip_id = 'Devtrac Trip (15074)'
         data = json.load(open(os.path.join(self.fixtures_dir,
                                            'site_visit_report.json')))
+        response = utils.process_json_submission(self.drupal, data,
+                                                 fieldtrip_id)
+        self.assertIsInstance(response, dict)
+        self.assertIn('uri', response)
+        self.assertIn('nid', response)
+
+    def test_process_roadside_submission(self):
+        self._drupal_login()
+
+        fieldtrip_id = 'Devtrac Trip (15074)'
+        data = json.load(open(os.path.join(self.fixtures_dir,
+                                           'roadside_report.json')))
+        response = utils.process_json_submission(self.drupal, data,
+                                                 fieldtrip_id)
+        self.assertIsInstance(response, dict)
+        self.assertIn('uri', response)
+        self.assertIn('nid', response)
+        warnings.warn(response['uri'])
+
+    def test_process_human_interest_submission(self):
+        self._drupal_login()
+
+        fieldtrip_id = 'Devtrac Trip (15074)'
+        data = json.load(open(os.path.join(self.fixtures_dir,
+                                           'human_interest.json')))
         response = utils.process_json_submission(self.drupal, data,
                                                  fieldtrip_id)
         self.assertIsInstance(response, dict)
